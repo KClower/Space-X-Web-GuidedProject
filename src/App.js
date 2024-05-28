1cf2f5ba74328de840146148ac2f9bd0213fdafd
KClower <kclower3@gmail.com>
2024-05-28 13:44:41 -0500

    made title shorter

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
   };
   return (
     <div className="App">
-      <h1>Space Missions are good and below us in a list</h1>
+      <h1>Space Missions are in a list below</h1>
       <MissionForm getData={getData} isFetchingData={isFetchingData} />
       <MissionsList error={error} missions={missions} />
     </div>
